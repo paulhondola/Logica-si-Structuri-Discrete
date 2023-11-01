84f760b89e372eee5d3ca4bdcb7b7266d00cd6f2
LilPolPol <paulhondola@gmail.com>
2023-11-01 17:09:54 +0200

    Update tema6.py

--- tema/tema6.py
+++ tema/tema6.py
@@ -11,7 +11,7 @@ def standard_partition(mul, condition):
     multime2 = mul - multime1
     return multime1, multime2      
     
-print(standard_partition({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, lambda x: x % 2 == 0))
+print("Standard partition:", standard_partition({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, lambda x: x % 2 == 0))
 
 
 # 5. Scrieți o funcție care ia o lista de mulțimi (de exemplu, de șiruri), și returnează reuniunea (variantă: intersectia) mulțimilor.
@@ -19,16 +19,24 @@ print(standard_partition({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, lambda x: x % 2 == 0))
 # Input: [{1, 2, 3}, {1, 2, 3, 4}, {3, 5, 6, 7}]; Output: reuniune: {1, 2, 3, 4, 5, 6, 7}; intersectie: {3}
 
 # TODO 5
- 
-    
-#print(reuniune([{1, 2, 3}, {1, 2, 3, 4}, {3, 5, 6, 7}]))
 
+lst = [{1, 2, 3}, {1, 2, 3, 4}, {3, 5, 6, 7}]
 
+def reuniune(lst):
+    if len(lst) == 0:
+        return set()
+    else:
+        return set(lst[0]) | reuniune(lst[1:])
 
+print("Reuniune:", reuniune(lst))
 
+def intersectie(lst):
+    if len(lst) == 0:
+        return set()
+    else:
+        return set(lst[0]) & intersectie(lst[1:])
 
-
-
+print("Intersectie:", intersectie(lst))
 
 # 6. Scrieți o funcție care returnează mulțimea cifrelor unui număr. Scrieți apoi altă funcție care ia o mulțime de numere și returnează reuniunea/intersecția dintre mulțimile cifrelor lor.
 
@@ -36,11 +44,26 @@ print(standard_partition({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, lambda x: x % 2 == 0))
 
 # TODO 6
 
+mul = {1234, 123, 127}
 
+def make_set_decimals(num):
+    if num == 0:
+        return {0}
+    else:
+        def f_aux(aux):
+            if aux == 0:
+                return set()
+            else:
+                return {aux % 10} | f_aux(aux // 10)
+    
+        return f_aux(num); 
 
-
-
-
-
-
-
+def reunion_decimals(my_set):
+    return functools.reduce(lambda acc, item: acc | make_set_decimals(item), my_set, set()) 
+     
+def intersection_decimals(my_set):
+    my_universe = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}           
+    return functools.reduce(lambda acc, item: acc & make_set_decimals(item), my_set, my_universe) 
+     
+print("Reunion:", reunion_decimals(mul))
+print("Intersection:", intersection_decimals(mul))
